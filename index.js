7dc59800cb4c00a2b2468112d44e064afb341e81
deepdhar <dhar2017.slg@gmail.com>
2025-02-19 17:36:36 +0530

    added scrollbehavior and corrected quizzo responsiveness

--- index.js
+++ index.js
@@ -3,4 +3,12 @@ let mybutton = document.getElementById("myBtn");
 function topFunction() {
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-  }
+}
+
+function goToProjects() {
+    const currentSection = document.activeElement.closest('section');
+    const nextSection = currentSection.nextElementSibling;
+    if (nextSection) {
+        nextSection.scrollIntoView();
+      }
+}
